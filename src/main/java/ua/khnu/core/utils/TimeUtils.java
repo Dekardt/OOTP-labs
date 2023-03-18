0aabbc043a9fb57469c293bf17a266299dfe70e6
Dekardt <sasha32083@gmail.com>
2023-03-18 14:50:23 +0200

    Lab2: Added TimeUtils

---
+++ src/main/java/ua/khnu/core/utils/TimeUtils.java
@@ -0,0 +1,31 @@
+package ua.khnu.core.utils;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.log4j.Log4j2;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
+@Log4j2
+@UtilityClass
+public class TimeUtils {
+
+    public static final Duration FIVE_SECONDS = Duration.ofSeconds(5);
+    public static final Duration TEN_SECONDS = Duration.ofSeconds(10);
+    public static final Duration HALF_MINUTE = Duration.ofSeconds(30);
+    public static final Duration ONE_MINUTE = Duration.ofSeconds(60);
+
+
+    public void waitFor(Duration duration) {
+        try {
+            Thread.sleep(duration.get(ChronoUnit.MILLIS));
+        } catch (InterruptedException e) {
+            log.error("Exception during wait for {%s}".formatted(duration), e);
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public void waitInSeconds(int seconds) {
+        waitFor(Duration.ofSeconds(seconds));
+    }
+}
